6f0c7e9007c62747349d64093aac980cc889fe01
damnardev <184518315+damnardev@users.noreply.github.com>
2024-12-09 18:18:33 +0100

    feat: REST Controller + Basic Auth (#15)

---
+++ rest-controller-basic-auth/src/main/java/fr/damnardev/configuration/InMemoryAuthWebSecurityConfigurer.java
@@ -0,0 +1,37 @@
+package fr.damnardev.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class InMemoryAuthWebSecurityConfigurer {
+
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		var user = User.withUsername("user")
+				.password(encoder.encode("password"))
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests((matcher) ->
+						matcher.anyRequest().authenticated()
+				)
+				.httpBasic(Customizer.withDefaults());
+		return http.build();
+	}
+
+}
