6d2f2556a5eb800d45ddf2418547b5a9c4de2da9
damnardev <184518315+damnardev@users.noreply.github.com>
2025-01-20 20:30:29 +0100

    feat: application runner (#54)

---
+++ application-runner/src/main/java/fr/damnardev/controller/ARRHandler.java
@@ -0,0 +1,18 @@
+package fr.damnardev.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ARRHandler implements ApplicationRunner {
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		log.info("Application runner started");
+	}
+
+}
