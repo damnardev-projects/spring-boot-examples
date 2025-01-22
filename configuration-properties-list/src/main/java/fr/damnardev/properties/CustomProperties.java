5ebd61bbf68cad6858800e658f66a15230f7089a
damnardev <184518315+damnardev@users.noreply.github.com>
2025-01-22 19:33:02 +0100

    feat: list properties

---
+++ configuration-properties-list/src/main/java/fr/damnardev/properties/CustomProperties.java
@@ -0,0 +1,21 @@
+package fr.damnardev.properties;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+public class CustomProperties {
+
+	private String id;
+
+	private List<String> names;
+
+}
