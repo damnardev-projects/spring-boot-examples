37da362356299184022faf79cd60c6cd278564e8
damnardev <184518315+damnardev@users.noreply.github.com>
2025-01-29 19:49:04 +0100

    fix build

--- jpa-simple-entity-with-one-to-one/src/main/java/fr/damnardev/entity/DbAddress.java
+++ jpa-simple-entity-with-one-to-one/src/main/java/fr/damnardev/entity/DbAddress.java
@@ -1,17 +1,10 @@
 package fr.damnardev.entity;
 
-import java.util.Map;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapKeyColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
