131d8cf7a91c505cf4132ff4e8989bbe6283cb20
damnardev <220764247+damnardev@users.noreply.github.com>
2026-08-30 21:54:34 +0200

    feat: add Scalar with REST controller example

--- buildSrc/src/main/java/fr/damnardev/gradle/plugins/DependenciesConvention.java
+++ buildSrc/src/main/java/fr/damnardev/gradle/plugins/DependenciesConvention.java
@@ -19,6 +19,7 @@ public class DependenciesConvention {
 	public void apply(Project project, VersionCatalog libs) {
 		Provider<MinimalExternalModuleDependency> springBootBom = get(libs, "spring-boot-bom");
 		Provider<MinimalExternalModuleDependency> swaggerUi = get(libs, "swagger-ui");
+		Provider<MinimalExternalModuleDependency> scalarUi = get(libs, "scalar-ui");
 
 		DependencyHandler dependencies = project.getDependencies();
 		dependencies.add(IMPLEMENTATION_CONFIGURATION_NAME, dependencies.platform(springBootBom));
@@ -30,6 +31,7 @@ public class DependenciesConvention {
 		// 4. Reference in module build.gradle: implementation libs.your.new.dependency
 
 		dependencies.constraints(c -> c.add(IMPLEMENTATION_CONFIGURATION_NAME, swaggerUi));
+		dependencies.constraints(c -> c.add(IMPLEMENTATION_CONFIGURATION_NAME, scalarUi));
 	}
 
 }
