5c18bbdb630aa6a9abc3fe6cfe07785da4b46d52
Damien <220764247+damnardev@users.noreply.github.com>
2026-09-02 17:54:50 +0200

    refactor: rename projects & packages (#16)

---
+++ projects/swagger-webmvc/src/main/java/fr/damnardev/spring/example/swaggerwebmvc/model/Employee.java
@@ -0,0 +1,55 @@
+package fr.damnardev.spring.example.swaggerwebmvc.model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Employee {
+
+	private UUID id;
+	private String firstName;
+	private String lastName;
+	private LocalDate birthday;
+
+	public Employee() {
+	}
+
+	public Employee(String firstName, String lastName, LocalDate birthday) {
+		this.id = UUID.randomUUID();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthday = birthday;
+	}
+
+	public UUID getId() {
+		return this.id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public LocalDate getBirthday() {
+		return this.birthday;
+	}
+
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
+
+}
